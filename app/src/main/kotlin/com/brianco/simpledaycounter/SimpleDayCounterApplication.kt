58163076311e65b2ad70ee5c54b5c3a55d287b5c
Eric Cochran <Eric@NightlyNexus.com>
2025-02-03 22:44:37 -0500

    Automatic whitespace cleanup.

--- app/src/main/kotlin/com/brianco/simpledaycounter/SimpleDayCounterApplication.kt
+++ app/src/main/kotlin/com/brianco/simpledaycounter/SimpleDayCounterApplication.kt
@@ -2,7 +2,7 @@ package com.brianco.simpledaycounter
 
 import android.app.Application
 
-class SimpleDayCounterApplication:Application() {
+class SimpleDayCounterApplication : Application() {
   internal lateinit var widgetDataSaver: WidgetDataSaver
 
   override fun onCreate() {
